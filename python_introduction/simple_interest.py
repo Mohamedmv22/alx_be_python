56a02c4556aa3c16e622c06fa1fbba85f7548922
Mohamed <vevomohamed0@gmail.com>
2025-11-06 13:41:20 +0200

    Add simple_interest.py file

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,12 @@
+#principal رئس المال , rate معدل الفائدة السنوي, time الوقت
+
+principal = 1000
+rate = 0.05 #5%
+time = 3
+
+interest = 1000 * 0.05 * 3
+total_amount = principal + interest
+
+print("Interest: ", interest)
+print("Total amount after 3 years: ", total_amount)
+
