f8796f7496f855c0c21f765ddbb9448352287dec
Mohamed <vevomohamed0@gmail.com>
2025-11-12 13:05:41 +0200

    Fixed the name issue

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,14 +1,14 @@
 sum1 = int(input("Enter the first number: "))
 sum2 = int(input("Enter the second number: "))
-operation = input("Choose the operation (+, -, *, /): ")
+operations = input("Choose the operation (+, -, *, /): ")
 
-if operation == "+":
+if operations == "+":
     print(f"The result is: {sum1+sum2}")
-elif operation == "-":
+elif operations == "-":
     print(f"The result is: {sum1-sum2}")
-elif operation == "*":
+elif operations == "*":
     print(f"The result is: {sum1*sum2}")
-elif operation == "/":
+elif operations == "/":
     print(f"The result is: {sum1/sum2}")
 else:
     print("Sorry, check the operation and try again.")
