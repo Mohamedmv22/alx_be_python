c2fbc8901f6f70092616141f97f3e531bfefa30a
Mohamedmv22 <vevomohamed0@gmail.com>
2025-11-23 19:53:42 +0200

    Fixed the space error

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,5 +1,5 @@
 
-def  perform_operation (num1, num2, operation):
+def  perform_operation(num1, num2, operation):
 
     if num2 == 0 and operation == "divide":
         return "Cannot divide by zero"
