195c610802b12f2824542975ada7f08d23b5ae75
Mohamedmv22 <vevomohamed0@gmail.com>
2025-11-23 19:56:39 +0200

    Fix num2 == 0

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,8 +1,9 @@
 
 def  perform_operation(num1, num2, operation):
 
-    if num2 == 0 and operation == "divide":
+    if num2 == 0:
         return "Cannot divide by zero"
+
 
     if operation == "add":
         return num1 + num2
