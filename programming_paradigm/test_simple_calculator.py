f9e9dcfd9fe94ef543c2e343440006bce0b49642
Mohamedmv22 <vevomohamed0@gmail.com>
2025-11-30 23:02:47 +0200

    test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,7 +6,6 @@ class TestSimpleCalculator(unittest.TestCase):
         self.calc = SimpleCalculator()
 
     def test_addition(self):
-
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertAlmostEqual(self.calc.add(2.5, 0.5), 3.0)
@@ -14,37 +13,31 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(10**6, 1), 10**6 + 1)
 
     def test_subtraction(self):
-
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(3, 5), -2)
         self.assertAlmostEqual(self.calc.subtract(2.5, 0.5), 2.0)
         self.assertEqual(self.calc.subtract(0, 0), 0)
 
     def test_multiplication(self):
-        
         self.assertEqual(self.calc.multiply(4, 5), 20)
         self.assertEqual(self.calc.multiply(-2, 3), -6)
         self.assertEqual(self.calc.multiply(-2, -3), 6)
         self.assertEqual(self.calc.multiply(0, 100), 0)
         self.assertAlmostEqual(self.calc.multiply(2.5, 2), 5.0)
 
-    def test_division_normal(self):
-  
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 5), 2.0)
         self.assertAlmostEqual(self.calc.divide(7, 2), 3.5)
         self.assertAlmostEqual(self.calc.divide(-6, 3), -2.0)
         self.assertAlmostEqual(self.calc.divide(1, 3), 1.0/3.0, places=7)
-
-    def test_division_by_zero(self):
-
         self.assertIsNone(self.calc.divide(5, 0))
         self.assertIsNone(self.calc.divide(0, 0))
 
     def test_more_edge_cases(self):
-
         self.assertAlmostEqual(self.calc.add(1, 2.2), 3.2)
         self.assertAlmostEqual(self.calc.multiply(0.1, 0.2), 0.02, places=7)
         self.assertAlmostEqual(self.calc.divide(10**12, 10**6), 10**6)
 
 if __name__ == "__main__":
     unittest.main()
+
